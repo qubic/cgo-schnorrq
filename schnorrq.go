0ae6f55ddb09f17d485314ebac6e8b947c52ebec
0xluk <luk@dev>
2024-04-04 23:59:10 +0200

    changed to dlfcn

--- schnorrq.go
+++ schnorrq.go
@@ -1,30 +1,55 @@
 package schnorrq
 
 /*
-#cgo CFLAGS: -g -Wall
-#cgo LDFLAGS: -L./lib -lfourqsign -Wl,-rpath=./lib
-#include "fourqsign.h"
-
-void signTransaction(const unsigned char* subseed, const unsigned char* publicKey, const unsigned char* messageDigest, unsigned char* signature);
+#cgo LDFLAGS: -ldl
+#include <dlfcn.h>
 */
 import "C"
+import (
+	"path/filepath"
+	"runtime"
+	"unsafe"
+)
 
-func Sign(subseed [32]byte, pubKey [32]byte, messageDigest [32]byte) [64]byte {
-	// Call the C function to sign the transaction
-	var signature [64]C.uchar
-	C.signTransaction((*C.uchar)(&subseed[0]), (*C.uchar)(&pubKey[0]), (*C.uchar)(&messageDigest[0]), &signature[0])
+// Declare function prototypes for dynamically loaded functions
+type signFunc func(subseed, publicKey, messageDigest, signature unsafe.Pointer)
+type verifyFunc func(publicKey, messageDigest, signature unsafe.Pointer) C.int
 
-	// Convert the signature from C type to Go type
-	var signatureGo [64]byte
-	for i := 0; i < len(signatureGo); i++ {
-		signatureGo[i] = byte(signature[i])
+var (
+	sign   signFunc
+	verify verifyFunc
+)
+
+func init() {
+	// Get the path of the current file
+	_, filename, _, _ := runtime.Caller(0)
+	dir := filepath.Dir(filename)
+	// Load the shared library dynamically
+	lib := C.dlopen(C.CString(filepath.Join(dir, "libfourq-qubic.so")), C.RTLD_LAZY)
+	if lib == nil {
+		panic("Failed to load shared library")
 	}
 
-	return signatureGo
+	// Load the necessary functions from the shared library
+	signTransactionSymbol := C.CString("sign")
+	verifySymbol := C.CString("verify")
+	sign = (signFunc)(C.dlsym(lib, signTransactionSymbol))
+	verify = (verifyFunc)(C.dlsym(lib, verifySymbol))
+
+	// Free the C strings used for symbol names
+	C.free(unsafe.Pointer(signTransactionSymbol))
+	C.free(unsafe.Pointer(verifySymbol))
 }
 
-//func Verify(pubKey [32]byte, messageDigest [32]byte, signature [64]byte) bool {
-//	ok := C.verify((*C.uchar)(&pubKey[0]), (*C.uchar)(&messageDigest[0]), (*C.uchar)(&signature[0]))
-//
-//	return bool(ok)
-//}
+// Sign function calls the dynamically loaded sign function
+func Sign(subseed [32]byte, pubKey [32]byte, messageDigest [32]byte) [64]byte {
+	var signature [64]byte
+	sign(unsafe.Pointer(&subseed[0]), unsafe.Pointer(&pubKey[0]), unsafe.Pointer(&messageDigest[0]), unsafe.Pointer(&signature[0]))
+	return signature
+}
+
+// Verify function calls the dynamically loaded verify function
+func Verify(pubKey [32]byte, messageDigest [32]byte, signature [64]byte) bool {
+	ok := verify(unsafe.Pointer(&pubKey[0]), unsafe.Pointer(&messageDigest[0]), unsafe.Pointer(&signature[0]))
+	return ok != 0
+}
