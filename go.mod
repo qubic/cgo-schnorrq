4d83fc19fc855cd69ea333845e1112faa736f9ba
0xluk <luk@dev>
2024-04-05 22:27:43 +0200

    changed sign to accept seed instead of subseed

--- go.mod
+++ go.mod
@@ -1,1 +1,13 @@
 module github.com/qubic/cgo-schnorrq
+
+go 1.20
+
+require (
+	github.com/pkg/errors v0.9.1
+	github.com/qubic/go-node-connector v0.4.2
+)
+
+require (
+	github.com/cloudflare/circl v1.3.6 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- schnorrq.go
+++ schnorrq.go
@@ -1,55 +1,36 @@
 package schnorrq
 
 /*
-#cgo LDFLAGS: -ldl
-#include <dlfcn.h>
+#cgo CFLAGS: -g -Wall
+#cgo LDFLAGS: -L. -lfourq-qubic -Wl,-rpath,$ORIGIN
+#include "fourq-qubic.h"
 */
 import "C"
 import (
-	"path/filepath"
-	"runtime"
-	"unsafe"
+	"github.com/pkg/errors"
+	"github.com/qubic/go-node-connector/types"
 )
 
-// Declare function prototypes for dynamically loaded functions
-type signFunc func(subseed, publicKey, messageDigest, signature unsafe.Pointer)
-type verifyFunc func(publicKey, messageDigest, signature unsafe.Pointer) C.int
+func Sign(seed string, pubKey [32]byte, messageDigest [32]byte) ([64]byte, error) {
+	subseed, err := types.GetSubSeed(seed)
+	if err != nil {
+		return [64]byte{}, errors.Wrap(err, "getting subseed")
+	}
+	// Call the C function to sign the transaction
+	var signature [64]C.uchar
+	C.sign((*C.uchar)(&subseed[0]), (*C.uchar)(&pubKey[0]), (*C.uchar)(&messageDigest[0]), &signature[0])
 
-var (
-	sign   signFunc
-	verify verifyFunc
-)
-
-func init() {
-	// Get the path of the current file
-	_, filename, _, _ := runtime.Caller(0)
-	dir := filepath.Dir(filename)
-	// Load the shared library dynamically
-	lib := C.dlopen(C.CString(filepath.Join(dir, "libfourq-qubic.so")), C.RTLD_LAZY)
-	if lib == nil {
-		panic("Failed to load shared library")
+	// Convert the signature from C type to Go type
+	var signatureGo [64]byte
+	for i := 0; i < len(signatureGo); i++ {
+		signatureGo[i] = byte(signature[i])
 	}
 
-	// Load the necessary functions from the shared library
-	signTransactionSymbol := C.CString("sign")
-	verifySymbol := C.CString("verify")
-	sign = (signFunc)(C.dlsym(lib, signTransactionSymbol))
-	verify = (verifyFunc)(C.dlsym(lib, verifySymbol))
-
-	// Free the C strings used for symbol names
-	C.free(unsafe.Pointer(signTransactionSymbol))
-	C.free(unsafe.Pointer(verifySymbol))
+	return signatureGo, nil
 }
 
-// Sign function calls the dynamically loaded sign function
-func Sign(subseed [32]byte, pubKey [32]byte, messageDigest [32]byte) [64]byte {
-	var signature [64]byte
-	sign(unsafe.Pointer(&subseed[0]), unsafe.Pointer(&pubKey[0]), unsafe.Pointer(&messageDigest[0]), unsafe.Pointer(&signature[0]))
-	return signature
-}
-
-// Verify function calls the dynamically loaded verify function
 func Verify(pubKey [32]byte, messageDigest [32]byte, signature [64]byte) bool {
-	ok := verify(unsafe.Pointer(&pubKey[0]), unsafe.Pointer(&messageDigest[0]), unsafe.Pointer(&signature[0]))
-	return ok != 0
+	ok := C.verify((*C.uchar)(&pubKey[0]), (*C.uchar)(&messageDigest[0]), (*C.uchar)(&signature[0]))
+
+	return bool(ok)
 }
